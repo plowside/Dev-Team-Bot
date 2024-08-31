005eb11611bd5312e87c04d3f62675787a39433c
plowside <plowsidee@gmail.com>
2024-08-31 23:25:53 +0700

    Update logging.py

--- app/utils/logging.py
+++ app/utils/logging.py
@@ -1,14 +1,14 @@
 # -*- coding: utf- 8 -*-
 import logging, os
 
-logs_path = 'logs/latest.log'
+logs_path = 'logs'
 os.makedirs(logs_path, exist_ok=True)
 # Формат логгирования
 log_formatter_file = logging.Formatter(u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s')
 log_formatter_console = logging.Formatter(u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s')
 
 # Логгирование в файл tgbot/data/logs.log
-file_handler = logging.FileHandler(logs_path, 'w', 'utf-8')
+file_handler = logging.FileHandler(f'{logs_path}/latest.log', 'w', 'utf-8')
 file_handler.setFormatter(log_formatter_file)
 file_handler.setLevel(logging.INFO)
 
